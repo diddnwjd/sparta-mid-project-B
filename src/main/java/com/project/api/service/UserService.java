482edb549c558b23df7958b715cfa85056e5f993
diddnwjd <yang965273@gmail.com>
2023-01-03 19:32:42 +0900

    [작업] 로그인 기능 구현

--- src/main/java/com/project/api/service/UserService.java
+++ src/main/java/com/project/api/service/UserService.java
@@ -1,8 +1,10 @@
 package com.project.api.service;
 
 import com.project.api.dto.CreateUserRequest;
+import com.project.api.dto.LoginUserRequest;
 import com.project.api.entity.User;
 import com.project.api.entity.UserRoleEnum;
+import com.project.api.jwt.JwtUtil;
 import com.project.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
     private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Transactional
@@ -33,5 +36,22 @@ public class UserService {
         }
         User user = new User(createUserRequest.getUsername(), createUserRequest.getPassword(), createUserRequest.getRole());
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public String loginUser(LoginUserRequest loginUserRequest) {  // void -> JwtInfo
+        String username = loginUserRequest.getUsername();
+        String password = loginUserRequest.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        if (!user.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String generatedToken = jwtUtil.createToken(user.getUsername(), user.getRole());   // 로그인 관련된 비즈니스 처리는 여기서
+        return generatedToken;
     }
 }
